5da82a9df244f381cb385a480f9fb7133295a382
Psycho X Ripper <157335958+professorxripper@users.noreply.github.com>
2024-02-05 13:07:11 +0600

    add two numbers by taking input from user

---
+++ add two number by taking input from user/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+    int b;
+    printf("Enter first number: ");
+    scanf("%d",&a);
+    printf("Enter second number: ");
+    scanf("%d",&b);
+    printf("The result of two numbers addition is: %d", a+b);
+    return 0;
+}
