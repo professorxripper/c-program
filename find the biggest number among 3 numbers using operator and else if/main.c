cba0e291b54d38a428a6169533303f9da20567fe
Psycho X Ripper <157335958+professorxripper@users.noreply.github.com>
2024-02-05 13:25:45 +0600

    compare 3 numbers using operator and else if

---
+++ find the biggest number among 3 numbers using operator and else if/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+    int b;
+    int c;
+    printf("Enter first number: ");
+    scanf("%d",&a);
+    printf("Enter second number: ");
+    scanf("%d",&b);
+    printf("Enter third number: ");
+    scanf("%d",&c);
+    if(a > b && a > c)
+    {
+        printf("First number is the largest number");
+    }
+    else if(b > a && b > c)
+    {
+        printf("Second number is the largest number");
+    }
+    else
+    {
+        printf("Third number is the largest number");
+    }
+    return 0;
+}
